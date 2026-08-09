25e024f1534497745606f58c9cf52b8c75a861ae
John Paul Salcedo <tiktokv806@gmail.com>
2026-08-09 11:21:52 +0800

    Add proxy list fetching from ProxyScrape

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import requests
 
-response = requests.get("https://api.ipify.org?format=json")
-data = response.json()
-print(f"Your Real IP Address is {data['ip']}")
+
+def get_proxy_list():
+    url = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=5000&country=all&ssl=all&anonymity=all"
+    response =requests.get(url)
+    proxy_list =response.text.strip().split("\r\n")
+    return proxy_list
+
+proxies = get_proxy_list()
+print(f"Found {len(proxies)} proxies")
+for proxy in proxies [:5]:
+    print(proxy)
